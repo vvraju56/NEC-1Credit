f1eb963e109bc8c7e3a9dad673f4ed89cdb6ecd4
V.V RAJU <132449380+vvraju56@users.noreply.github.com>
2025-03-03 23:47:09 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,22 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Personal API"
+
+@app.route('/name')
+def name():
+    return "Name: vishnuraju v"
+
+@app.route('/register_number')
+def register_number():
+    return "Register Number: 22IT056"
+
+@app.route('/department')
+def department():
+    return "Department: Information Technology"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  
